e63a10f7f9091455f323eb75d0def4ab1fee2893
Elmi Elmi <elmielmi522@gmail.com>
2019-12-24 11:07:34 -0600

    setup DetailsPage.js

---
+++ src/components/Detailspage/Detailspage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Detailspage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Detailspage;
