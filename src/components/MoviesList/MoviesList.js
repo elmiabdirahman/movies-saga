f88564ad323dd5d1ff0d48765a0d3800a29ec1f8
Elmi Elmi <elmielmi522@gmail.com>
2019-12-24 10:54:29 -0600

    setup movieslist.js

---
+++ src/components/MoviesList/MoviesList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MoviesList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MoviesList;
