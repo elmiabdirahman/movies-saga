0e9b8a5b8d89b2d7c8f0f185080314bebd4c8e9f
Elmi Elmi <elmielmi522@gmail.com>
2019-12-24 11:17:29 -0600

    update DetailsPage.js

--- src/components/Detailspage/Detailspage.js
+++ src/components/Detailspage/Detailspage.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class Detailspage extends Component {
+
+    componentDidMount = () => {
+        this.getMovieDetail()
+    }
+
+    getMovieDetail = () => {
+        this.props.dispatch({ type: 'GET_DETAIL', payload: this.props.match.params })
+    }
+    
+
     render() {
         return (
             <div>
-                
+                <div>
+                    {this.props.reduxState.genres.map((movie) =>
+                        <div key={movie.id}>
+                            <p>{movie.title}</p>
+                            <img src={movie.poster} alt={movie.description} />
+                            <p>{movie.description}</p>
+                            <p>GENRE: {movie.genre_list}</p>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
