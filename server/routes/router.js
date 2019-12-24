8a63731ca4a704a441b295e69e9361d37f8e4475
Elmi Elmi <elmielmi522@gmail.com>
2019-12-24 17:18:54 -0600

    update server routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -13,4 +13,23 @@ router.get('/', (req, res)=>{
     })
 });
 
+router.get('/:id', (req, res) =>{
+    const queryGenre = `SELECT "movies".id, "movies".title, 
+                        "movies".poster, "movies".description, 
+                        string_agg("genres".name, ', ') AS genre_list FROM "movies"
+                        JOIN "movies_genres"
+                        ON "movies".id = "movies_genres".movies_id
+                        JOIN "genres"
+                        ON "genres".id = "movies_genres".genres_id		
+                        WHERE "movies".id = $1
+                        GROUP BY 1;`;
+                      
+    pool.query(queryGenre, [req.params.id]).then(( results ) =>{
+        res.send(results.rows);
+    }).catch( (error) =>{
+        console.log('Error completing SELECT movies quert ', error);
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router
