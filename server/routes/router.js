85302b2eb67e39df69b17cc59e63a9ef709d1dfc
Elmi Elmi <elmielmi522@gmail.com>
2019-12-24 10:50:10 -0600

    added get route

---
+++ server/routes/router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    const queryMovies = 'SELECT id, title, poster, description FROM "movies";';
+    pool.query(queryMovies).then(( results ) =>{
+        res.send(results.rows);
+    }).catch( (error) =>{
+        console.log('Error completing SELECT movies quert ', error);
+        res.sendStatus(500);
+    })
+});
